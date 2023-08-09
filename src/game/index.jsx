c8d12ea4fe971a85a8cb839b8df74c1438343b9f
Tiago Freitas <70216659+FreitsTiago@users.noreply.github.com>
2023-08-08 22:41:40 -0300

    Game finalizado
    alterações da estrutura principal de funcionamento do game para adequar ao React pois estou usando pela primeira vez

--- src/game/index.jsx
+++ src/game/index.jsx
@@ -1,39 +1,99 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './styles.css'
 
-import circle from '../assets/circle.svg'
-import x from '../assets/x.svg'
+import O from '../assets/circle.svg'
+import X from '../assets/x.svg'
 
 function Game() {
-  const [score, setScore] = useState([0,0])
-  const [playit, setPlayit] = useState(1)
-  const [game_table, setGame_table] = useState([])
+  const emptyBoard = Array(9).fill('')
 
-  function game_start() {
-    document.querySelector('#game_status').innerHTML('X começa')
+  const [board, setBoard] = useState(emptyBoard)
+  const [currentPlayer, setCurrentPlayer] = useState('X')
+  const [winner, setWinner] = useState('')
+  const [score, setScore] = useState([0, 0])
+
+  const handleCellClick = (index) => {
+    if (board[index] !== '') return null
+    if (winner) return null
+    setBoard(board.map((item, itemIndex) => itemIndex === index ? currentPlayer : item))
+    setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X')
+  }
+
+  const checkWinner = () => {
+    let l_winner = false
+    const possibleWaysToWin = [
+      [board[0], board[1], board[2]],
+      [board[3], board[4], board[5]],
+      [board[6], board[7], board[8]],
+
+      [board[0], board[3], board[6]],
+      [board[1], board[4], board[7]],
+      [board[2], board[5], board[8]],
+
+      [board[0], board[4], board[8]],
+      [board[2], board[4], board[6]],
+    ]
+
+    possibleWaysToWin.forEach(cells => {
+      if (cells.every(cell => cell === 'O')) {
+        setWinner('O')
+        setScore([score[0], score[1] + 1])
+        l_winner = true
+      } else if (cells.every(cell => cell === 'X')) {
+        setWinner('X')
+        setScore([score[0] + 1, score[1]])
+        l_winner = true
+      } 
+    })
+
+    if(!l_winner) {
+      let couter = 0
+      board.forEach(cell => {
+        if(cell !== '') couter ++
+      })
+      if(couter === 9) {
+        setWinner('EMPATE')
+      }
+    }
+  }
+
+  useEffect(checkWinner, [board])
+
+  const resetGame = () => {
+    setBoard(emptyBoard)
+    setCurrentPlayer('X')
+    setWinner('')
   }
 
   return (
     <div className='container'>
       <div className='scoreboard'>
-        <div id='score_x'>
-          <img src={x} />
+        <div className={currentPlayer === 'X' && winner === '' ? 'playit' : ''}>
+          <img src={X} />
           <h1>{score[0]}</h1>
         </div>
-        <h1 id='game_status'></h1>
-        <div id='score_o'>
-          <img src={circle} />
+        <div className={currentPlayer === 'O' && winner === '' ? 'playit' : ''}>
+          <img src={O} />
           <h1>{score[1]}</h1>
         </div>
       </div>
-      <div className='game_table' id='game_table'>
-        {Array.from({ length: 9 }).map((_, index) => (
-          <button key={index}><img src={game_table[index] == 1 ? x : game_table[index] == 2 ? circle : undefined} /></button>
+      <div className={`game_table ${winner !== '' ? 'desable' : ''}`}>
+        {board.map((item, index) => (
+          <div className={`cell ${item}`} key={index} onClick={() => handleCellClick(index)}><img src={item === 'X' ? X : item === 'O' ? O : ''} /></div>
         ))}
       </div>
-      <div className='action_board'>
-          <button id='game_starter' onClick={game_start()}>START</button>
-      </div>
+      {winner &&
+        <div className='action_board'>
+          <button onClick={() => resetGame()}>Recomeçar jogo!</button>
+        </div>
+      }
+      {winner &&
+        <div className='winnerBanner'>
+          <h1>{winner === 'EMPATE' ? 'Empate!' : 'Vitória!'}</h1>
+          <img src={winner === 'X' ? X : winner === 'O' ? O : ''} />
+        </div>
+      }
+
     </div>
   )
 }
